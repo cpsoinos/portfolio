124ad9591d02f6827f00be0f2d283986518a1282
Corey Psoinos <coreypsoinos@gmail.com>
2024-06-29 23:02:20 -0400

    remove Dompurify for now so it works with cloudflare

--- src/lib/components/resume/Experience.svelte
+++ src/lib/components/resume/Experience.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import type { ExperienceItem } from './types';
-	import DOMPurify from 'isomorphic-dompurify';
 
 	export let items: ExperienceItem[];
 	export let sectionTitle: string;
@@ -30,7 +29,7 @@
 				{#if item.description}
 					<p class="text-sm print:text-2xs print:leading-normal">
 						<!-- eslint-disable-next-line svelte/no-at-html-tags -->
-						{@html DOMPurify.sanitize(item.description)}
+						{@html item.description}
 					</p>
 				{/if}
 				{#if item.bulletPoints?.length}
@@ -38,7 +37,7 @@
 						{#each item.bulletPoints as bulletPoint}
 							<li class="text-sm text-gray-900 print:text-2xs print:leading-normal">
 								<!-- eslint-disable-next-line svelte/no-at-html-tags -->
-								{@html DOMPurify.sanitize(bulletPoint)}
+								{@html bulletPoint}
 							</li>
 						{/each}
 					</ul>
