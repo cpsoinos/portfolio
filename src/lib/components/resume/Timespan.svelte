edc784e57dc449ea01570bde1f9ef12b474dcfae
Corey Psoinos <coreypsoinos@gmail.com>
2024-07-02 09:11:44 -0400

    feat(resume): 2 col style, add skills section, update wording (#8)
    * --wip-- [skip ci]
    
    * configure alignment for 2 column style
    
    * more styling
    
    * wrap links in projects
    
    * add skills section to right column
    
    * tweak wording
    
    * use sm as breakpoint to switch to 2 cols
    
    * fix mobile styles and display

---
+++ src/lib/components/resume/Timespan.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let startDate: Date;
+	export let endDate: Date | undefined = undefined;
+</script>
+
+<span class="text-xs text-indigo-600 print:text-2xs">
+	{startDate.toLocaleDateString('en-US', { month: 'short', year: 'numeric' })} - {endDate?.toLocaleDateString(
+		'en-US',
+		{ month: 'short', year: 'numeric' }
+	) || 'current'}
+</span>
