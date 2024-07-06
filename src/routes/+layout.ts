0a2cf4ed468d2a96fd7258ffe0cd7961084ef37a
Corey Psoinos <coreypsoinos@gmail.com>
2024-07-06 15:18:41 -0400

    add posthog-js and setup with custom reverse proxy

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,18 @@
 import { preloadIcons } from '$lib/icons';
 import type { LayoutLoad } from './$types';
+import { browser } from '$app/environment';
+import posthog from 'posthog-js';
+import { env } from '$env/dynamic/public';
 
 export const load: LayoutLoad = async () => {
 	await preloadIcons();
+
+	if (browser) {
+		posthog.init(env.PUBLIC_POSTHOG_API_KEY, {
+			api_host: 'https://ph.anderapps.com',
+			person_profiles: 'always' // or 'always' to create profiles for anonymous users as well
+		});
+	}
+
 	return {};
 };
