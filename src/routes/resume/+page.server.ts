b74bdc631dbad767eaf557883ccf83c581e6470d
Corey Psoinos <coreypsoinos@gmail.com>
2024-07-31 17:04:49 -0400

    fix: use octokit and gql to get stargazers

--- src/routes/resume/+page.server.ts
+++ src/routes/resume/+page.server.ts
@@ -1,13 +1,27 @@
+import { env } from '$env/dynamic/private';
 import { projects } from '$lib/projects';
 import type { PageServerLoad } from './$types';
+import { Octokit } from '@octokit/core';
+
+const octokit = new Octokit({ auth: env.GITHUB_TOKEN });
 
 export const load: PageServerLoad = async () => {
 	const projectsDataPromises = projects.map(async (project) => {
 		if (project.github) {
-			const github = project.github;
-			const res = await fetch(`https://api.github.com/repos/${github.username}/${github.repo}`);
-			const data = await res.json();
-			project.github.stars = data.stargazers_count;
+			const {
+				repository: { stargazerCount }
+			} = await octokit.graphql<{ repository: { stargazerCount: number } }>(
+				`query stargazersCount($owner: String!, $repo: String!) {
+      repository(owner: $owner, name: $repo) {
+        stargazerCount
+      }
+    }`,
+				{
+					owner: project.github.username,
+					repo: project.github.repo
+				}
+			);
+			project.github.stars = stargazerCount;
 		}
 		if (project.npm) {
 			const npm = project.npm;
