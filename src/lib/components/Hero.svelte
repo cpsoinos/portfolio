6d795876063efc30512b1a9a1ce06ea239132392
Corey Psoinos <coreypsoinos@gmail.com>
2024-08-01 18:48:00 -0400

    serve hero img from cloudflare

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+	import { buildImageUrl, TRANSFORM_BASE_URL } from '$lib/images';
+	import { Image } from '@unpic/svelte';
+
 	const title = "Hi. I'm Corey.";
 	const subtitle =
 		"I'm a full-stack software enginer with a passion for great, accessible UX. I love writing beautiful code that makes beautiful designs come to life.";
+
+	const heroImg = buildImageUrl('hero-illustration.png');
 </script>
 
 <section class="grid grid-cols-3 items-center py-40">
@@ -9,5 +14,5 @@
 		<h1 class="font-display text-4xl text-slate-50">{title}</h1>
 		<p class="font-mono text-slate-200">{subtitle}</p>
 	</div>
-	<img class="col-span-1" src="/hero.png" alt="Illustration of Corey" />
+	<Image class="col-span-1" width={420} height={440} src={heroImg} alt="Illustration of Corey" />
 </section>
