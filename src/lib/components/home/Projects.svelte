7eb13576e94a68aebe824ceecdbe3d8f45296d90
Corey Psoinos <coreypsoinos@gmail.com>
2024-08-07 22:13:01 -0400

    remove description from projects section

--- src/lib/components/home/Projects.svelte
+++ src/lib/components/home/Projects.svelte
@@ -5,7 +5,7 @@
 
 <section id="projects" class="flex flex-col gap-6">
 	<h2 class="font-display text-4xl text-slate-900 dark:text-slate-50">Projects</h2>
-	<p class="font-mono">Here are some of the projects I've worked on outside of work.</p>
+	<!-- <p class="font-mono">Here are some of the projects I've worked on outside of work.</p> -->
 
 	<div class="grid gap-8 md:grid-cols-2">
 		{#each projects as project}
